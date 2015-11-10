f08b679ba38528deed29347bf900b5df219c471b
Sahil Dhanju <sahildhanju1@gmail.com>
2015-11-10 17:01:36 -0600

    Set up basic layout and .gitignore

---
+++ index.js
@@ -0,0 +1,15 @@
+express = require("express");
+
+app = express();
+
+app.set('port', (process.env.PORT) || 5000)	;
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", function(req, res)
+{
+	res.render('index.html');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
