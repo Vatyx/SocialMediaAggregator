69d321fcf57db0ca7876ebc1705b30eab26a8d1b
Sahil Dhanju <sahildhanju1@gmail.com>
2015-11-13 00:03:55 -0600

    Finally fixed stylus

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-/*
- * Module dependencies
- */
-var express = require('express')
-  , stylus = require('stylus')
-  , nib = require('nib')
+var express = require("express");
+var stylus = require('stylus');
+var nib = require('nib');
 
-
-var app = express()
+var app = express();
 
 function compile(str, path) {
   return stylus(str)
@@ -14,48 +10,22 @@ function compile(str, path) {
     .use(nib());
 }
 
-app.set('views', __dirname + '/views')
 app.use(stylus.middleware(
-  { src: __dirname + '/public'
+  { src: __dirname + '/resources'
+  ,	dest: __dirname + '/public'
   , compile: compile
   }
-))
-app.use(express.static(__dirname + '/public'))
+));
 
-app.get('/', function (req, res) {
-  res.render('index.html');
-  
-})
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use(express.static(__dirname + '/public'));
 
-app.listen(3000)
+app.get("/", function(req, res)
+{
+	res.render('index.html');
+});
 
-// var express = require("express");
-// var stylus = require('stylus');
-// var nib = require('nib');
-
-// var app = express();
-
-// function compile(str, path) {
-//   return stylus(str)
-//     .set('filename', path)
-//     .use(nib());
-// }
-
-// app.use(stylus.middleware(
-//   { src: __dirname + '/public'
-//   , compile: compile
-//   }
-// ));
-
-// app.use('/bower_components',  express.static(__dirname + '/bower_components'));
-// app.use(express.static(__dirname + '/public'));
-
-// app.get("/", function(req, res)
-// {
-// 	res.render('index.html');
-// });
-
-// app.set('port', (process.env.PORT) || 5000)	;
-// app.listen(app.get('port'), function() {
-//   console.log('Node app is running on port', app.get('port'));
-// });
+app.set('port', (process.env.PORT) || 5000)	;
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
