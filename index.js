44610a45ca4d4c061e4a48e2608fed8736b25e1d
Sahil Dhanju <sahildhanju1@gmail.com>
2015-11-12 23:47:45 -0600

    Trying to make stylus work

--- index.js
+++ index.js
@@ -1,33 +1,61 @@
-var express = require("express");
-var stylus = require('stylus');
-var nib = require('nib');
+/*
+ * Module dependencies
+ */
+var express = require('express')
+  , stylus = require('stylus')
+  , nib = require('nib')
 
-var app = express();
 
-function compile(str, path)
-{
-	return stylus(str)
-		.set('filename', path)
-		.set('compress', true)
-		.use(nib());
+var app = express()
+
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib());
 }
 
-app.set('port', (process.env.PORT) || 5000)	;
-app.use(express.static(__dirname + '/public'));
-app.use('/bower_components',  express.static(__dirname + '/bower_components'));
-
+app.set('views', __dirname + '/views')
 app.use(stylus.middleware(
-	{	
-		src: __direname + '/public'
-	,	compile: compile
-	}
+  { src: __dirname + '/public'
+  , compile: compile
+  }
 ))
+app.use(express.static(__dirname + '/public'))
 
-app.get("/", function(req, res)
-{
-	res.render('index.html');
-});
+app.get('/', function (req, res) {
+  res.render('index.html');
+  
+})
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+app.listen(3000)
+
+// var express = require("express");
+// var stylus = require('stylus');
+// var nib = require('nib');
+
+// var app = express();
+
+// function compile(str, path) {
+//   return stylus(str)
+//     .set('filename', path)
+//     .use(nib());
+// }
+
+// app.use(stylus.middleware(
+//   { src: __dirname + '/public'
+//   , compile: compile
+//   }
+// ));
+
+// app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+// app.use(express.static(__dirname + '/public'));
+
+// app.get("/", function(req, res)
+// {
+// 	res.render('index.html');
+// });
+
+// app.set('port', (process.env.PORT) || 5000)	;
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
