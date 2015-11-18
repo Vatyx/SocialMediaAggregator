e875ad0ff3143fd229e0fdb9c0a449b02848f28b
Sahil Dhanju <sahildhanju1@gmail.com>
2015-11-17 23:27:11 -0600

    Playing around with spacing

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.use('/', routes);
 
-app.set('port', (process.env.PORT) || 5000)	;
+app.set('port', (process.env.PORT) || 3000)	;
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
