59a92d404e7bdc01c8dc5dd266111fc00cdafd93
Sahil Dhanju <sahildhanju1@gmail.com>
2016-01-12 23:35:23 -0600

    Creating API

--- routes/instagram.js
+++ routes/instagram.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
 router.get("/auth", function(req, res)
@@ -8,23 +9,26 @@ router.get("/auth", function(req, res)
 
 router.get("/callback", function(req, res)
 {
-	request('http://www.google.com', function (error, response, body) {
-	  	if (!error && response.statusCode == 200) {
-	    	console.log(body) // Print the google web page.
-		}
-	});
-
 	var getMedia = function(error, response, body)
 	{
-
+		console.log("Got in here");
+		console.log(response);
+		if (!error && response.statusCode == 200) {
+		    console.log(body); 
+		}
+		else
+		{
+			console.log(error);
+		}
 	}
 
-	request("https://api.instagram.com/v1/users/self/", getMedia);
-
+	request("https://api.instagram.com/v1/users/self/?access_token=22ddbc4e16be4d52a31e6ed38b644d9d" + req.query.code, function(error, response, body)
+		{
+			console.log("Im here");
+		});
+	console.log(req.query.code);
 	res.render('index');
 	
 });
-
-console.log("In here");
 
 module.exports = router;
